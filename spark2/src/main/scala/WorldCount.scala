f689ebc608179dbf835fe89e62a7121fc6c727ba
killer11112 <1093084958@qq.com>
2020-04-09 02:28:31 +0800

    tow push

---
+++ spark2/src/main/scala/WorldCount.scala
@@ -0,0 +1,13 @@
+import org.apache.spark.{SparkConf, SparkContext}
+object WorldCount {
+  def main(args: Array[String]): Unit = {
+    val logFile = "F:\\spark-2.4.4-bin-hadoop2.7\\hello.txt"
+    val conf = new SparkConf().setAppName("wordcount").setMaster("local")
+    val sc = new SparkContext(conf)
+    val rdd = sc.textFile(logFile)
+    val counts = rdd.flatMap(line=>line.split(" ")).map(x=>(x,1)).reduceByKey((x,y)=>(x+y))
+    counts.foreach(println)
+    sc.stop()
+
+  }
+}
